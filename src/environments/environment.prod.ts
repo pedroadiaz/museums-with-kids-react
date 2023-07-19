be77f13e9681a56fc32e4842353d0aa5124b830c
pdiaz-afs <88383851+pdiaz-afs@users.noreply.github.com>
2023-07-19 02:45:10 -0700

    Removing client secret

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
   production: true,
+  NX_DOMAIN: "dev-v2dp1knv.us.auth0.com",
+  NX_CLIENT_ID: "zuZchk7o8LYC7NJrsYoZVHrBC9N096Tj",
+  NX_SERVER_URL: "http://www.museumswithkids.com",
+  NX_API_URL: "https://api.museumswithkids.com",
 };
